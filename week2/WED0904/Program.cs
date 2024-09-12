efdc2da0704d4c47f66c00836827f695e82ed888
Nicolás Rufino <nicolasrufino@Mateos-MacBook-Pro.local>
2024-09-12 13:18:05 -0600

    done

--- week2/WED0904/Program.cs
+++ week2/WED0904/Program.cs
@@ -17,3 +17,30 @@
 
 List<int> listOfInts = new List<int> { 7, 24, 7, 2, 1, 6, 3, 2, 4 };
 
+//  int countInterface(char x, string s)
+// {
+//     int count = 0;
+//     foreach (char c in s)
+//     {
+//         if (c == x)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+ static int recursiveSum(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n + recursiveSum(n - 1);
+    }
+}
+
+Console.WriteLine(recursiveSum(1000));
